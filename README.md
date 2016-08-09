6bb20bf636b6d6bee170bea28610488312a3a211
Jeff <jeffalstott@gmail.com>
2016-08-09 14:46:02 +0800

    Added raw data and inital model.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # technologytimeseries
+
+
+
+
+Two Projects (Could be separate papers)
+----
+Data Science 
+Science
+
+
+Data Science
+----
+Replicate with maximum likelihood
+Replicate with Bayes
+Test with additional data set (from Benson and Magee)
+Group parameters (this is the relationship for the Science (see below) )
+Group level predictors? (e.g. genomics vs. chemicals vs. energy vs. …. ; price vs. non-price)
+Box-Cox transformation?
+...
+
+Science
+----
+Model relationship between speed and variability
+Do all the simulations that possibly explain speed and variability
---
+++ src/Random_Walk_Fitting.py
@@ -0,0 +1,139 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import seaborn as sns
+import pystan
+get_ipython().magic('pylab inline')
+
+
+# In[2]:
+
+data_directory = '../data/'
+
+
+# In[26]:
+
+time_series = pd.read_csv(data_directory+'time_series.csv', index_col=0)
+time_series_metadata = pd.read_csv(data_directory+'time_series_metadata.csv', index_col=0)
+
+
+# In[6]:
+
+arma_model_code = """
+data { 
+int<lower=1> T; 
+real y[T];
+}
+
+parameters { 
+real mu; 
+//real phi;
+real<lower=-1,upper=1> theta; 
+real<lower=0> sigma; 
+} 
+
+transformed parameters{
+real K;
+K <- sqrt( (1+theta^2) * sigma^2 );
+}
+
+model {
+real err; 
+mu ~ normal(0,10); 
+theta ~ uniform(-1,1); //normal(0,2); 
+sigma ~ cauchy(0,5); 
+err <- y[1] - 2*mu; //phi = 1
+err ~ normal(0,sigma); 
+for (t in 2:T){ 
+    err <- y[t] - (mu + y[t-1] + theta * err); 
+    err ~ normal(0,sigma);
+    }
+}
+"""
+
+# model {
+# vector[T] nu; // prediction for time t
+# vector[T] err; // error for time t
+ 
+# nu[1] <- mu + phi * mu; // assume err[0] == 0 
+# err[1] <- y[1] - nu[1]; 
+# for (t in 2:T) { 
+#     nu[t] <- mu + phi * y[t-1] + theta * err[t-1]; 
+#     err[t] <- y[t] - nu[t];
+#     } 
+
+# mu ~ normal(0,10); // priors
+# phi ~ normal(0,2); 
+# theta ~ normal(0,2); 
+# sigma ~ cauchy(0,5); 
+# err ~ normal(0,sigma); // likelihood
+# }
+
+
+# In[7]:
+
+arma_model = pystan.StanModel(model_code=arma_model_code)
+
+
+# In[28]:
+
+random_walk_parameters = pd.DataFrame(columns=['mu', 'K', 'theta', 'sigma'], dtype='float')
+
+for col in time_series.columns:
+    if time_series_metadata.ix[col,'Source']!='Farmer_Lafond':
+        continue
+    print(col)
+    d = time_series[col].dropna().values.astype('float')
+    data = {'y':log(d),
+       'T':len(d)}
+    fit = arma_model.sampling(data=data)
+    random_walk_parameters.ix[col, 'mu'] = fit['mu'].mean()
+    random_walk_parameters.ix[col, 'K'] = fit['K'].mean()
+    random_walk_parameters.ix[col, 'theta'] = fit['theta'].mean()
+    random_walk_parameters.ix[col, 'sigma'] = fit['sigma'].mean()
+# random_walk_parameters = random_walk_parameters.astype('float')
+
+
+# In[29]:
+
+random_walk_parameters['mu'].hist(bins=50)
+plot((0,0), ylim(), 'k--')
+title("Mu")
+figure()
+random_walk_parameters['K'].hist(bins=50)
+title("K")
+figure()
+random_walk_parameters['sigma'].hist(bins=50)
+title("sigma")
+figure()
+random_walk_parameters['theta'].hist(bins=50)
+title("theta")
+
+
+# In[30]:
+
+random_walk_log_parameters = log(random_walk_parameters)
+
+
+# In[31]:
+
+random_walk_parameters.plot('mu', 'sigma', kind='scatter')
+figure()
+random_walk_log_parameters.plot('mu', 'sigma', kind='scatter')
+
+
+# In[32]:
+
+random_walk_parameters.plot('mu', 'K', kind='scatter')
+figure()
+random_walk_log_parameters.plot('mu', 'K', kind='scatter')
+
+
+# In[33]:
+
+from scipy.stats import linregress
+linregress(random_walk_parameters['mu'], random_walk_parameters['K'])
+
